7c246f7db62e2195b2415e9aeec96099e14e250e
Huy <93818026+Heineken2001@users.noreply.github.com>
2025-02-10 21:38:50 +0700

    Initial Commit

---
+++ attestSwiftUi/attestSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  attestSwiftUiApp.swift
+//  attestSwiftUi
+//
+//  Created by Huy Tran Nhat on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct attestSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ attestSwiftUiTests/attestSwiftUiTests.swift
@@ -0,0 +1,16 @@
+//
+//  attestSwiftUiTests.swift
+//  attestSwiftUiTests
+//
+//  Created by Huy Tran Nhat on 10/2/25.
+//
+
+import Testing
+
+struct attestSwiftUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
